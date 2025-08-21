a0ba37159c0508b4fe0645c044e953eb246190ee
spacehare <dantath425@gmail.com>
2025-08-21 19:16:03 -0400

    add sfx; limit tower_base placement

--- game/selector.gd
+++ game/selector.gd
@@ -3,6 +3,8 @@ extends Node2D
 
 var _grabbed_object: Node2D = null
 @export var level: Level
+@onready var audio_error: AudioStreamPlayer2D = $AudioError
+@onready var audio_place: AudioStreamPlayer2D = $AudioPlace
 
 signal grabbed(thing)
 signal dropped(thing)
@@ -17,8 +19,13 @@ func _unhandled_input(event: InputEvent):
 	if event.is_action_released("act_select"):
 		if _grabbed_object:
 			if _grabbed_object is TowerBase:
-				pass
-			release()
+				var ok = _grabbed_object.can_place()
+				if ok:
+					release()
+				else:
+					audio_error.play()
+			else:
+				release()
 
 
 func has_item() -> bool:
@@ -46,3 +53,4 @@ func release():
 		dropped.emit(_grabbed_object)
 		_grabbed_object.reparent(level)
 		_grabbed_object = null
+		audio_place.play()
--- game/towers/tower_base.gd
+++ game/towers/tower_base.gd
@@ -1,4 +1,8 @@
 class_name TowerBase
 extends Node2D
 
-@onready var hitbox = $AreaHitbox
+@onready var hitbox: Area2D = $AreaHitbox
+
+
+func can_place() -> bool:
+	return not hitbox.has_overlapping_areas()
